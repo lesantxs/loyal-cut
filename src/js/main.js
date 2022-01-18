48189d9c039409bbc8548041fee1236a68f41861
LeoAChaves <leochaves2121@gmail.com>
2022-01-18 16:59:04 -0300

    implementando mudança de tema

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+//botão hamburguer
+
 const btnMobile = document.querySelector("#btn_mobile");
 
 function toggleMenu(e) {
@@ -17,3 +19,32 @@ function toggleMenu(e) {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+//mudança de tema
+
+const tema = document.querySelector("#switch");
+const html = document.querySelector("html");
+
+function mudarTema(e) {
+  if (e.type === "touchstart") {
+    e.preventDefault();
+  }
+  html.classList.toggle("dark-mode");
+
+  const dark = html.classList.contains("dark-mode");
+
+  const iconeTema = document.querySelector(".icone-switch");
+  const src = iconeTema.getAttribute("src");
+
+  if (dark) {
+    iconeTema.setAttribute("src", src.replace("moon", "sun"));
+    sessionStorage.temaAtual = "dark";
+  } else {
+    iconeTema.setAttribute("src", src.replace("sun", "moon"));
+    sessionStorage.temaAtual = "ligth";
+  }
+  console.log(sessionStorage.temaAtual);
+}
+
+tema.addEventListener("click", mudarTema);
+tema.addEventListener("touchstart", mudarTema);
